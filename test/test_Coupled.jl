ebbbcb042ed15448dfd6b26eaebd8dcde97ccec3
David Anthoff <anthoff@berkeley.edu>
2026-04-28 10:47:39 -0700

    Migrate to test items

--- test/test_Coupled.jl
+++ test/test_Coupled.jl
@@ -1,40 +1,42 @@
-using Mimi, MimiSSPs, DataFrames, CSVFiles, Query, Test
+@testitem "Coupled" begin
+    using Mimi, DataFrames, CSVFiles, Query
 
-dummy_input_output = load(joinpath(@__DIR__, "..", "data", "keys", "OECD Env-Growth_dummyInputOutput.csv")) |> DataFrame
+    dummy_input_output = load(joinpath(@__DIR__, "..", "data", "keys", "OECD Env-Growth_dummyInputOutput.csv")) |> DataFrame
 
-inputregions = dummy_input_output.Input_Region
-outputregions = sort(unique(dummy_input_output.Output_Region))
+    inputregions = dummy_input_output.Input_Region
+    outputregions = sort(unique(dummy_input_output.Output_Region))
 
-m = Model()
-set_dimension!(m, :time, 1750:2300)
+    m = Model()
+    set_dimension!(m, :time, 1750:2300)
 
-# Handle the MimiSSPs.SSPs component
-add_comp!(m, MimiSSPs.SSPs, first = 2010, last = 2300)
-set_dimension!(m, :country, inputregions)
-update_param!(m, :SSPs, :country_names, inputregions)
+    # Handle the MimiSSPs.SSPs component
+    add_comp!(m, MimiSSPs.SSPs, first = 2010, last = 2300)
+    set_dimension!(m, :country, inputregions)
+    update_param!(m, :SSPs, :country_names, inputregions)
 
-update_param!(m, :SSPs, :SSP_source, "OECD Env-Growth")
-update_param!(m, :SSPs, :SSP, "SSP1")
-update_param!(m, :SSPs, :emissions_source, "Leach")
-update_param!(m, :SSPs, :emissions_scenario, "SSP119")
+    update_param!(m, :SSPs, :SSP_source, "OECD Env-Growth")
+    update_param!(m, :SSPs, :SSP, "SSP1")
+    update_param!(m, :SSPs, :emissions_source, "Leach")
+    update_param!(m, :SSPs, :emissions_scenario, "SSP119")
 
-# Handle the MimiSSPs.RegionAggregatorSum component
-add_comp!(m, MimiSSPs.RegionAggregatorSum, first = 2010, last = 2300)
+    # Handle the MimiSSPs.RegionAggregatorSum component
+    add_comp!(m, MimiSSPs.RegionAggregatorSum, first = 2010, last = 2300)
 
-set_dimension!(m, :inputregions, inputregions)
-set_dimension!(m, :outputregions, outputregions)
+    set_dimension!(m, :inputregions, inputregions)
+    set_dimension!(m, :outputregions, outputregions)
 
-update_param!(m, :RegionAggregatorSum, :input_region_names, inputregions)
-update_param!(m, :RegionAggregatorSum, :output_region_names, outputregions)
-update_param!(m, :RegionAggregatorSum, :input_output_mapping, dummy_input_output.Output_Region)
+    update_param!(m, :RegionAggregatorSum, :input_region_names, inputregions)
+    update_param!(m, :RegionAggregatorSum, :output_region_names, outputregions)
+    update_param!(m, :RegionAggregatorSum, :input_output_mapping, dummy_input_output.Output_Region)
 
-connect_param!(m, :RegionAggregatorSum, :input, :SSPs, :population)
+    connect_param!(m, :RegionAggregatorSum, :input, :SSPs, :population)
 
-run(m)
+    run(m)
 
-# Should also work if Aggregator runs long, using backup data
-Mimi.set_first_last!(m, :RegionAggregatorSum, first = 1750)
-backup = zeros(551, 184)
-connect_param!(m, :RegionAggregatorSum, :input, :SSPs, :population, backup, ignoreunits=true)
+    # Should also work if Aggregator runs long, using backup data
+    Mimi.set_first_last!(m, :RegionAggregatorSum, first = 1750)
+    backup = zeros(551, 184)
+    connect_param!(m, :RegionAggregatorSum, :input, :SSPs, :population, backup, ignoreunits=true)
 
-run(m)
+    run(m)
+end
